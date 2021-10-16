f92acb5aecbcb7c239ece22530ad2d2318801f2e
YZolotovski <zolotovsky2004@gmail.com>
2021-10-16 21:38:23 +0300

    add second task in first lesson

---
+++ Lesson1/Task2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "  ,   ," << endl;
+    cout << " /////|" << endl;
+    cout << "///// |" << endl;
+    cout << "|~~~| |" << endl;
+    cout << "|===| |" << endl;
+    cout << "|j  | |" << endl;
+    cout << "| g | |" << endl;
+    cout << "|  s| /" << endl;
+    cout << "|===|/" << endl;
+    cout << "'---'" << endl;
+    return 0;
+}
