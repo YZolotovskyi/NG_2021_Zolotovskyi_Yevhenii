b0ed7abf02fdd94ec8a82a5986f56d6bbe4b919d
YZolotovski <zolotovsky2004@gmail.com>
2021-10-23 18:57:27 +0300

    add fourth task in first lesson with fix

--- Lesson1/Task4/main.cpp
+++ Lesson1/Task4/main.cpp
@@ -8,13 +8,16 @@ int main()
     int a;
     int b;
     int c;
+    cout << "Enter the number in front of the x square and press enter" << endl;
     cin >> a;
+    cout << "Enter the number before x and press enter" << endl;
     cin >> b;
+    cout << "Enter the third number and press enter" << endl;
     cin >> c;
     double d;
     d=(b*b)-(4*a*c);
     if (d>0){
-       int x1,x2;
+       double x1,x2;
        x1=(-b+sqrt(d))/(2*a);
        x2=(-b-sqrt(d))/(2*a);
        cout << "Two roots: " << ((x1>x2)? x2: x1) << " " << ((x1>x2)? x1: x2);
