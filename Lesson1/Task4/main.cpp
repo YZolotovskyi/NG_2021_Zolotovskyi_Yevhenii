9ac72885529173aa0f380fba66f0fb1d92c72034
YZolotovski <zolotovsky2004@gmail.com>
2021-11-03 10:26:30 +0300

    add fourth task in first lesson with fix

--- Lesson1/Task4/main.cpp
+++ Lesson1/Task4/main.cpp
@@ -15,14 +15,14 @@ int main()
     cout << "Enter the third number and press enter" << endl;
     cin >> c;
     double d;
+    double x1,x2;
     d=(b*b)-(4*a*c);
     if (d>0){
-       double x1,x2;
        x1=(-b+sqrt(d))/(2*a);
        x2=(-b-sqrt(d))/(2*a);
        cout << "Two roots: " << ((x1>x2)? x2: x1) << " " << ((x1>x2)? x1: x2);
     }
-       if (d==0){
+       else if (d==0){
           cout << " One root:" << " " << -b/(2*a) << endl;
        }
        else
--- Lesson1/Task6/main.cpp
+++ Lesson1/Task6/main.cpp
@@ -10,52 +10,35 @@ int main()
     cout << "255.255.255.*" << endl;
     cout << "Please enter your number instead of * " << endl;
     cin >> a;
+    cout << "number of available ip-addresses ";
     switch(a) {
     case 255:
-        cout << "number of available ip-addresses ";
         cout << "1";
         break;
-
     case 254:
-        cout << "number of available ip-addresses ";
         cout << "2";
         break;
-
-        case 252:
-        cout << "number of available ip-addresses ";
+    case 252:
         cout << "4";
         break;
-
     case 248:
-        cout << "number of available ip-addresses ";
         cout << "8";
         break;
-
-        case 240:
-        cout << "number of available ip-addresses ";
+    case 240:
         cout << "16";
         break;
-
     case 224:
-        cout << "number of available ip-addresses ";
         cout << "32";
         break;
-
-        case 192:
-        cout << "number of available ip-addresses ";
+    case 192:
         cout << "64";
         break;
-
     case 128:
-        cout << "number of available ip-addresses ";
         cout << "128";
         break;
-
-        case 000:
-        cout << "number of available ip-addresses ";
+    case 000:
         cout << "256";
         break;
-
     default:
         cout << "No roots" << endl;
     }
