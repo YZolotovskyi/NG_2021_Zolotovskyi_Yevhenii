4343420c6e5aa26c4657f27cdc0969ba5b63e3a3
YZolotovski <zolotovsky2004@gmail.com>
2021-11-03 10:32:22 +0300

    #include <iostream> with fix

--- Lesson1/Task5/main.cpp
+++ Lesson1/Task5/main.cpp
@@ -6,16 +6,13 @@ using namespace std;
 
 int main()
 {   double m;
-    int l;
     cout << "enter the mass of the ship"<< endl;
     cin >> m;
-    cout <<"enter the distance in light years" << endl;
-    cin >> l;
     m = m/3;
     m = ceil (m);
     m = m - 2;
-    m = m * l;
-    cout << "You need to" " ";
+    m = m * 300;
+    cout << "You need to ";
     cout << ( m );
     cout <<" " "fuel"<< endl;
     return 0;
